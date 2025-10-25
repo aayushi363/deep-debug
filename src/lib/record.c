c82896f016ee0d7696d0095fdeb92e50a5427902
aayushi363 <aayushi363@gmail.com>
2025-10-25 21:39:46 +0000

    saving two ckpt imgs

--- src/lib/record.c
+++ src/lib/record.c
@@ -3,6 +3,7 @@
 #include "mcmini/spy/checkpointing/objects.h"
 #include "mcmini/spy/checkpointing/transitions.h"
 #include "mcmini/spy/intercept/interception.h"
+#include "mcmini/wrapper_timing.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,6 +39,7 @@ rec_list *find_thread_record_mode(pthread_t thrd) {
 }
 
 rec_list *find_object_record_mode(void *addr) {
+  MEASURE_FUNCTION_TIME
   return find_object(addr, head_record_mode);
 }
 
@@ -60,6 +62,7 @@ rec_list *add_rec_entry(const visible_object *vo, rec_list **head, rec_list **cu
 }
 
 rec_list *add_rec_entry_record_mode(const visible_object *vo) {
+  MEASURE_FUNCTION_TIME
   rec_list *new_node = (rec_list *)malloc(sizeof(rec_list));
   if (new_node == NULL) {
     perror("malloc");
